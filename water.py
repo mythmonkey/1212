8ef7e3d0a945264ef78ade46379627c74db71c12
mythmonkey <myth.mandy@gmail.com>
2022-12-12 10:29:47 +0800

    20221212-2

---
+++ water.py
@@ -0,0 +1,21 @@
+from bs4 import BeautifulSoup
+import requests
+
+#搜尋目標位址
+url = "https://water.taiwanstat.com/"
+#向位址提出請求
+web = requests.get("https://water.taiwanstat.com/")
+
+if web.status_code == 200:
+    print("取得網頁內容成功") 
+       
+    soup = BeautifulSoup(web.text, "lxml")
+    
+    tag = soup.find_all("h3")
+    
+    for row in tag:
+        print(row.get_text("", strip=True))
+    
+            
+else:
+    print("取得網頁內容失敗")  
