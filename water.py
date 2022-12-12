559c129e0c2259b2b9128a05641180a1d0f4f651
mythmonkey <myth.mandy@gmail.com>
2022-12-12 14:33:49 +0800

    issue1 gogogo

--- water.py
+++ water.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 
 #搜尋目標位址
-url = "https://www.google.com/m?client=ms-android-sonymobile&channel=bm"
+url = "https://water.taiwanstat.com/"
 #向位址提出請求
 web = requests.get("https://water.taiwanstat.com/")
 
@@ -10,12 +10,12 @@ if web.status_code == 200:
     print("取得網頁內容成功") 
        
     soup = BeautifulSoup(web.text, "lxml")
-    
+    print("issue1 gogogo")
     tag = soup.find_all("h3")
     
     for row in tag:
         print(row.get_text("\n", strip=True))
-        print("issue1")
+        print("rebase")
     
             
 else:
